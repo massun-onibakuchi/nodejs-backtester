d75d152cd0060e759ab9284efb1b79ecad779b33
bakuchi- <helveticaf0015se@gmail.com>
2020-09-14 20:16:21 +0900

    fix backtrade class method and constructoer

--- backtest-pattern2.js
+++ backtest-pattern2.js
@@ -2,50 +2,24 @@
 
 function genBackTest(ohlcv, parentClass) {
     return new class BackTester extends parentClass {
-        constructor(allOhlcv, balance, commsion) {
-            super(ohlcv)
-            this.allOhlcv = allOhlcv.reverse()
-            this.length = ohlcv.length;
-            this.ohlcv = []
-            this.open = []
-            this.high = []
-            this.low = []
-            this.close = []
-            this.volume = []
-            this.position = { timestamp: 0, qty: 0, aveOpenPrice: 0 }
+        constructor(ohlcv) {
+            super(ohlcv);
         }
         runTest() {
             super.next()
         }
+        // sayhello(){console.log('fefeeef');}
         addOhlcv(ohlcv) {
             this.ohlcv = ohlcv
             console.log('backtsting');
         }
-        entry(qty) {
-            // this.position = { timestamp: length, qty: qty, aveOpenPrice: this.open[this.open.length - 1] }
-            this.position['timestamp'] = length;
-            const originQty = this.position['qty'];
-            this.position['aveOpenPrice'] = (this.position['aveOpenPrice'] * originQty + this.allOhlcv[this.length - 1][0] * qty) / (originQty + qty)
-            this.position['qty'] += qty;
-        }
-        buy() {
-            this.buyEntry = { timestamp: Date.now(), qty: 1, price: this.open[this.open.length - 1] }
-        }
-        sell() {
-            this.buyEntry = { timestamp: Date.now(), qty: 1, price: this.open[this.open.length - 1] }
-        }
-        //最後にnextで呼ぶ
-        updateOhlcv() {
-            const next = this.allOhlcv.pop();
-            this.Ohlcv.push(next); //nextOhlcv
-        }
     }(ohlcv)
 }
-//必要か？？backtest classにつければいいんじゃね>?
+
 class TradeManagement {
     constructor(allOhlcv, balance, commsion) {
         this.allOhlcv = allOhlcv.reverse()
-        this.length = ohlcv.length;
+        this.length = allOhlcv.length;
         this.ohlcv = []
         this.open = []
         this.high = []
@@ -63,10 +37,10 @@ class TradeManagement {
         this.position['qty'] += qty;
     }
     buy() {
-        this.buyEntry = { timestamp: Date.now(), qty: 1, price: this.open[this.open.length - 1] }
+        this.buyEntry = { timestamp: Date.now(), qty: 1, aveOpenPrice: this.open[this.open.length - 1] }
     }
     sell() {
-        this.buyEntry = { timestamp: Date.now(), qty: 1, price: this.open[this.open.length - 1] }
+        this.buyEntry = { timestamp: Date.now(), qty: 1, aveOpenPrice: this.open[this.open.length - 1] }
     }
     //最後にnextで呼ぶ
     updateOhlcv() {
@@ -77,19 +51,18 @@ class TradeManagement {
 }
 
 class Strategy extends TradeManagement {
-    // constructor(ohlcv, balance, commsion) {
-    //     super(ohlcv, balance, commsion)
-    //     this.profit = 0
-    //     this.loss = 0
-    //     this.dd = 0
-    // }
+    constructor(ohlcv, balance, commsion) {
+        super(ohlcv, balance, commsion)
+        this.profit = 0
+        this.loss = 0
+        this.dd = 0
+    }
 
     sma() { return this.ohlcv }
     //nextはStrategyには不要かも
     next() {
         // 
     }
-
     init() { }
 }
 
@@ -103,6 +76,15 @@ class MyStrategy extends Strategy {
         super.next()
         super.buy()
     }
+
+    // sayHello(){
+    //     console.log('call parent class"s method');
+    //     sayhello() // TypeError: (intermediate value).sayhello is not a function
+    // }
+    // callHoge(){
+    //     console.log(this.hoge);
+    // }
+
     // innjiとかの初期化
     init() { }
     /*     addOhlcv() {
@@ -117,10 +99,9 @@ console.log('bt :>> ', bt);
 console.log('bt instanceof MyStrategy :>> ', bt instanceof MyStrategy);
 console.log('bt instanceof Strategy :>> ', bt instanceof Strategy);
 console.log('bt.profit :>> ', bt.profit);
-console.log('bt.buyEntry :>> ', bt.buyEntry);
 console.log('bt.sma() :>> ', bt.sma());
 console.log('bt.next() :>> ', bt.next());
 console.log('bt.addOhlcv([0,32,21,1,2]) :>> ', bt.addOhlcv([0, 32, 21, 1, 2]));
 console.log('bt.ohlcv :>> ', bt.ohlcv);
 console.log('bt.buyEntry :>> ', bt.buyEntry);
-
+bt.callHoge()
